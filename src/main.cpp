dc5a950894e8fbbc8dab93f1489e829fb285a4c0
Alexander Koch <8818177+blackhawk92@users.noreply.github.com>
2019-07-11 12:02:09 +0200

    First try with Nextion

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,26 @@
 
 
 
-#define CALMIN 660
-#define CALMAX 285
+#define CALMIN 680
+#define CALMAX 280
 #define POTCOUNT 1
 #define HUMIDITY_THRESHOLD 40
 #define WATERING_DURATION 10000
 #define RECENT_THRESHOLD 60000
+#define DELAYTIME 10000
 
 int analogPins[] = {54,55,56,57};
 int pumpPWMPins[] = {2,3,4,5};
 int pumpDirPins[] = {22,24,26,28};
 unsigned long watered_at[POTCOUNT];
+char textBuffer[100] = {0};
 
-NexGauge humid_gauges[POTCOUNT];
-NexText ago_hours[POTCOUNT];
-NexText ago_mins[POTCOUNT];
+NexGauge humid_gauges[POTCOUNT] = {NexGauge(0,1, "z0")/*,NexGauge(0,3, "z1"),NexGauge(0,4, "z2"),NexGauge(0,6, "z3")*/};
+NexText ago_texts[POTCOUNT] = {NexText(0,2,"t0")/*,NexText(0,8,"t1"),NexText(0,5,"t2"),NexText(0,7,"t3")*/};
 
 void setup() {
   Serial.begin(9600);
-  //nexInit();
+  nexInit();
   
   for (int i = 0; i < POTCOUNT; i++)
   {
@@ -47,7 +48,27 @@ void pumpWater(int potNumber, int duration){
 
 int getHumidity(int potNumber){
   int percent = map(analogRead(analogPins[potNumber]),CALMIN,CALMAX,0,100);
+  //Serial.println(analogRead(analogPins[potNumber]));
   return percent;
+}
+
+int calcGaugeVal(int percent){
+  if (percent>100)
+  {
+    percent=100;
+  }else if (percent<0)
+  {
+    percent=0;
+  }
+  int gaugeVal = map(percent,0,100,0,270);
+  if (gaugeVal<=45)
+  {
+    gaugeVal += 315;
+  }else
+  {
+    gaugeVal -= 45;
+  }
+  return gaugeVal;
 }
 
 bool recentlyWatered(int potNumber){
@@ -63,24 +84,46 @@ bool recentlyWatered(int potNumber){
   return true;
 }
 
+void updateDisplay(int gaugeVals[], int humidities[]){
+  for (int i = 0; i < POTCOUNT; i++)
+  {
+    humid_gauges[i].setValue(gaugeVals[i]);
+    uint16_t numString = humidities[i];
+    memset(textBuffer, 0, sizeof(textBuffer));
+    itoa(numString, textBuffer, 10);
+    ago_texts[i].setText(textBuffer);
+  }
+}
+
 void loop() {
-  int percent = getHumidity(0);
-  Serial.println(percent);
-  Serial.println(millis());
-  //pumpWater(0, 10000);
-  delay(5000);
-  while (false)
+  while (true)
   {
     int humidities[POTCOUNT];
+    int gaugeVals[POTCOUNT];
     for (int i = 0; i < POTCOUNT; i++)
     {
       humidities[i] = getHumidity(i);
+      gaugeVals[i] = calcGaugeVal(humidities[i]);
       if(humidities[i] < HUMIDITY_THRESHOLD && !recentlyWatered(i)){
         pumpWater(i, WATERING_DURATION);
         watered_at[i] = millis();
       }
     }
-    //TODO: Variablen ans Display senden
+    updateDisplay(gaugeVals,humidities);
+    delay(DELAYTIME);
   }
+
+  int percent = getHumidity(0);
+  int humidities[POTCOUNT];
+  int gaugeVals[POTCOUNT];
+  for (int i = 0; i < POTCOUNT; i++)
+  {
+    humidities[i] = percent;
+    gaugeVals[i] = calcGaugeVal(humidities[i]);
+  }
+  updateDisplay(gaugeVals,humidities);
+
+  pumpWater(0, 10000);
+  delay(10000);
   
 }
